21f8fb139fbd0eb109ade7c50abf348797aaf262
Ahmed Naguib <aweal0122@gmail.com>
2025-07-27 13:17:06 +0300

    Clean up project structure and add new features

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,26 +1,62 @@
+// import db from "@/db/db";
+// import { notFound } from "next/navigation";
+// import Stripe from "stripe";
+// import { CheckoutForm } from "./_components/CheckoutForm";
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+// export default async function PurchasePage({
+//   params: { id },
+// }: {
+//   params: { id: string };
+// }) {
+//   const product = await db.product.findUnique({ where: { id } });
+//   if (product == null) return notFound();
+
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: product.priceInCents,
+//     currency: "USD",
+//     metadata: { productId: product.id },
+//   });
+
+//   if (paymentIntent.client_secret == null) {
+//     throw Error("Stripe failed to create payment intent");
+//   }
+
+//   return (
+//     <CheckoutForm
+//       product={product}
+//       clientSecret={paymentIntent.client_secret}
+//     />
+//   );
+// }
 import db from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
 import { CheckoutForm } from "./_components/CheckoutForm";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-06-30.basil",
+});
 
-export default async function PurchasePage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  const product = await db.product.findUnique({ where: { id } });
-  if (product == null) return notFound();
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function PurchasePage({ params }: PageProps) {
+  const product = await db.product.findUnique({ where: { id: params.id } });
+  if (!product) return notFound();
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: product.priceInCents,
-    currency: "USD",
+    currency: "usd",
     metadata: { productId: product.id },
   });
 
-  if (paymentIntent.client_secret == null) {
-    throw Error("Stripe failed to create payment intent");
+  if (!paymentIntent.client_secret) {
+    throw new Error("Stripe failed to create payment intent");
   }
 
   return (
